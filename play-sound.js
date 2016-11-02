cdab13fd409aeb90ca702b262c0f4e5265d23ac3
Loïc Nogues <nogues.loic@gmail.com>
2016-11-02 14:34:12 +0100

    Actually playing a local .ogg/vorbis sound

--- play-sound.js
+++ play-sound.js
@@ -1,8 +1,17 @@
 //const youtubedl = require('youtube-dl');
 var fs = require('fs');
 var ytdl = require('ytdl-core');
+var ogg = require('ogg');
+var vorbis = require('vorbis');
+var Speaker = require('speaker');
 
-const uri = 'o8yiYCHMAlM';
+// Create the Speaker instance
+var speaker = new Speaker({
+  channels: 2,          // 2 channels
+  bitDepth: 16,         // 16-bit samples
+  sampleRate: 44100     // 44,100 Hz sample rate
+});
+const uri = '2DMEUhqB_t4';
 
 //const video = youtubedl(uri, ['--format=251']);
 
@@ -12,8 +21,31 @@ const uri = 'o8yiYCHMAlM';
 
 //video.pipe(function(stream) {
 //});
-ytdl(`http://www.youtube.com/watch?v=${uri}`, { filter: 'audioonly', filter: function(format) {
-  console.log(format.audioEncoding, format.audioBitrate);
-  return (format.audioEncoding == 'vorbis');
-}})
-  .pipe(fs.createWriteStream('video.flv'));
+
+var od = new ogg.Decoder();
+od.on('stream', function (stream) {
+  var vd = new vorbis.Decoder();
+
+  // the "format" event contains the raw PCM format
+  vd.on('format', function (format) {
+    // send the raw PCM data to stdout
+    vd.pipe(speaker);
+  });
+
+  // an "error" event will get emitted if the stream is not a Vorbis stream
+  // (i.e. it could be a Theora video stream instead)
+  vd.on('error', function (err) {
+    // maybe try another decoder...
+  });
+
+  stream.pipe(vd);
+});
+
+fs.createReadStream(`${__dirname}/audio.ogg`).pipe(od);
+
+//ytdl(`http://www.youtube.com/watch?v=${uri}`, { filter: function(format) {
+//  console.log(format.audioEncoding, format.audioBitrate, format.container, format.resolution);
+//  return (format.audioEncoding == 'vorbis' && format.resolution == null);
+//}})
+//  //.pipe(ogg);
+//  .pipe(fs.createWriteStream('audio.webm'));
